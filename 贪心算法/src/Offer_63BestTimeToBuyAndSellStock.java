93a74da2741781e73acc08b49e5c617219358cfe
21seu <1693376841@qq.com>
2022-08-06 17:55:29 +0800

    剑指Offer63：买卖股票的最佳时机（mid）

---
+++ 贪心算法/src/Offer_63BestTimeToBuyAndSellStock.java
@@ -0,0 +1,23 @@
+/**
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0
+ *
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ */
+public class Offer_63BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+        //贪心
+        int minPirce = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int p : prices) {
+            if (p < minPirce) minPirce = p;
+            else profit = Math.max(profit, p - minPirce);
+        }
+        return profit;
+    }
+}
