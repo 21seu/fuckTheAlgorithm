3482e00b6469f71faa86c88e4b08b3a09a32c6c9
21seu <1693376841@qq.com>
2022-09-18 13:02:30 +0800

    feat：LC_541反转字符串2（easy）

--- 字符串&数组/src/LC_541ReverseStringII.java
+++ 字符串&数组/src/LC_541ReverseStringII.java
@@ -1,18 +1,43 @@
 /**
  * 给定一个字符串 s 和一个整数 k，从字符串开头算起，每 2k 个字符反转前 k 个字符。
- *
+ * <p>
  * 如果剩余字符少于 k 个，则将剩余字符全部反转。
  * 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
- *
+ * <p>
  * 输入：s = "abcdefg", k = 2
  * 输出："bacdfeg"
- *
+ * <p>
  * 输入：s = "abcd", k = 2
  * 输出："bacd"
  */
 public class LC_541ReverseStringII {
 
     public String reverseStr(String s, int k) {
-        return null;
+        int len = s.length();
+        int part = len / (2 * k);
+        int part_part = len % (2 * k);
+        part_part = (part_part >= k && part_part < 2 * k) ? k : part_part;
+        int start = 0;
+        while (part-- > 0) {
+            s = reverse(s, start, start + k - 1);
+            start += (2 * k);
+        }
+        return part_part != 0 ? reverse(s, start, start + part_part - 1) : s;
+    }
+
+    private String reverse(String s, int start, int end) {
+        char[] chArr = s.toCharArray();
+        while (start <= end) {
+            char temp = chArr[start];
+            chArr[start] = chArr[end];
+            chArr[end] = temp;
+            start++;
+            end--;
+        }
+        return new String(chArr);
+    }
+
+    public static void main(String[] args) {
+        new LC_541ReverseStringII().reverseStr("abcd", 2);
     }
 }
