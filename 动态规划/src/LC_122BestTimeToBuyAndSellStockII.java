fda1ac8f0585d9d0075eddba63e0da9e301d19b1
21seu <1693376841@qq.com>
2022-10-07 23:43:30 +0800

    feat：LC_25 K个一组反转链表，递归解法（hard）

---
+++ 动态规划/src/LC_122BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+/**
+ * 给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+ * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+ * 返回 你能获得的 最大 利润 。
+ * 示例 1：
+ * 输入：prices = [7,1,5,3,6,4]
+ * 输出：7
+ * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
+ *      随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6 - 3 = 3 。
+ * 总利润为 4 + 3 = 7 。
+ * 示例 2：
+ * 输入：prices = [1,2,3,4,5]
+ * 输出：4
+ * 解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
+ *      总利润为 4 。
+ */
+public class LC_122BestTimeToBuyAndSellStockII {
+
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+
+        // 0：持有现金
+        // 1：持有股票
+        // 状态转移：0 → 1 → 0 → 1 → 0 → 1 → 0
+        int[][] dp = new int[len][2];
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < len; i++) {
+            // 这两行调换顺序也是可以的
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            System.out.println("利润:" + dp[i][0]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            System.out.println("股价:" + dp[i][1]);
+        }
+        return dp[len - 1][0];
+    }
+
+    public static void main(String[] args) {
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        new LC_122BestTimeToBuyAndSellStockII().maxProfit(prices);
+    }
+}
